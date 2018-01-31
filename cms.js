30b3cdad63307e9e9a478a53f1edf5a256489c75
Jacob Knaack <jacobknck@gmail.com>
2018-01-30 16:15:16 -0800

    fixed issue with url logic

--- cms.js
+++ cms.js
@@ -31,7 +31,7 @@ const url = window.location.href;
 let queryString = url.substring(url.indexOf('?') + 4);
 
 for ( var item in articles) {
-  if (!url.includes('article')) {
+  if (document.getElementById("submissions")) {
     document.getElementById("submissions").innerHTML += "<li class='indexListItem'><a id='article" + articles[item].id + "' class='listAnchor' href='/template/article.html?id=" + articles[item].id + "'><i class='fa fa-html5 indexListImg' class aria-hidden='true'></i>"+articles[item].name+"</a></li>"
   }
   if (url.includes('article') && parseInt(queryString) === articles[item].id) {
