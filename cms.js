866808a4d097eb23ce12a096cf8f51936c8900f5
Jacob Knaack <jacobknck@gmail.com>
2018-02-01 13:07:25 -0800

    added content to article1, tweaked styling, needs to be styled for small screens

--- cms.js
+++ cms.js
@@ -3,6 +3,7 @@ const articles = [
     "id": 1,
     "created_on": "Jan 30, 2018",
     "name": "Building a Chat UI with basic HTML, CSS, and Javascript: Part 1",
+    "description": "Taking an idea from prototype to a working interface runnning in a Browser, with tools available to every modern computer user.",
     "html": "<p class='subtitle'>This post is focused on basic web development and design methodology. The goal is to help people take a prototype from concept to a development build running on a local computer. But the build will be simple enough to push to any hosting service without any fancy tools.</p>\
             <p>Let\'s build a really simple Chat UI, one that has all the features of a deployed chat application, but runs in the browser with nothing but three files:</p>\
             <ul class='articleList'>\
@@ -27,16 +28,30 @@ const articles = [
             <p class='textNumbers'>2) The list of messages in the middle section</p>\
             <p class='textNumbers'>3) The input section at the bottom of the middle section</p>\
             <div class='divider'></div>\
-            <h3 class='contentHeading'>Now we can Create our files</h3>\
-            <p>Now create a folder or directory anywhere on your computer, name it whatever you like, and create three files in that directory, one titled index.html, another titled style.css, and a third titled chat.js. Here's my directory structure after my files are created:</p>\
+            <h3 class='contentHeading'>Now we can Begin to Structure Our Project</h3>\
+            <p>Create a folder or directory anywhere on your computer, name it whatever you like, and create three files in that directory, one titled index.html, another titled style.css, and a third titled chat.js. Here's my directory structure after my files are created:</p>\
             <code class='codeBlock'>\
-              project<br>\
+              project-folder<br>\
               |- index.html<br>\
               |- style.css<br>\
               |_ chat.js<br>\
             </code>\
-            <p>Now Lets open up our html file in a text editor of your choosing and begin laying out our template for our chat interface:</p>\
-            <p></p>"  
+            <p>With those files created, let open up our HTML file and begin to lay our the structure of the interface</p>\
+            <p>Here is the boilerplate html code we want to start out with:</p>\
+            <code class='codeBlock'>\
+              index.html<br><br>\
+              1&nbsp &lt;!DOCTYPE html&gt; <br>\
+              2&nbsp &lt;html&gt; <br>\
+              3&nbsp &nbsp&nbsp &lt;head&gt; <br>\
+              4&nbsp &nbsp&nbsp&nbsp&nbsp &lt;meta charset=\"UTF-8\"&gt; <br>\
+              5&nbsp &nbsp&nbsp&nbsp&nbsp &lt;meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"&gt; <br>\
+              6&nbsp &nbsp&nbsp &lt;head&gt; <br>\
+              7&nbsp &nbsp&nbsp &lt;body&gt;<br>\
+              8&nbsp &nbsp&nbsp &lt;body&gt;<br>\
+              9&nbsp &lt;html&gt;\
+            </code>\
+            <p>From here will add some html between the body tags. The code we add here will only be used to give some shape to our UI. The important bits we will add using mocked data and javascript.</p>\
+            <p>When we begin putting HTML in the body of the document, we must think about each element as a box.  The opening tag is the top of the box and the closing tag is the bottom of the box, and each preceding tag we put between those tags is another little box: </p>"  
   },
 ];
 
@@ -46,7 +61,7 @@ let queryString = url.substring(url.indexOf('?') + 4);
 
 for ( var item in articles) {
   if (document.getElementById("submissions")) {
-    document.getElementById("submissions").innerHTML += "<li class='indexListItem'><a id='article" + articles[item].id + "' class='listAnchor' href='"+url+"template/article.html?id=" + articles[item].id + "'><i class='fa fa-code indexListImg' class aria-hidden='true'></i>"+articles[item].name+"</a></li>"
+    document.getElementById("submissions").innerHTML += "<li class='indexListItem'><p class='date'>"+articles[item].created_on+"</p><a id='article" + articles[item].id + "' class='listAnchor' href='"+url+"template/article.html?id=" + articles[item].id + "'><i class='fa fa-code indexListImg' class aria-hidden='true'></i>"+articles[item].name+"</a><p class='articleDescription'>"+articles[item].description+"</p></li>"
   }
   if (url.includes('article') && parseInt(queryString) === articles[item].id) {
     document.getElementById('articleTitle').innerHTML = articles[item].name;
--- template/sideNav.js
+++ template/sideNav.js
@@ -4,5 +4,5 @@ document.getElementById("sideNav").innerHTML =
     <p class='description'>\
       Jacob is a Web Develeper out of Seattle WA, when he isn\'t programming the internet of things, he is drawing, teaching, reviewing movies, and doing video game things.\
     </p>\
-    <a href='../' class='homeBtn'><i class='fa fa-home' aria-hidden='true'></i></a>\
+    <a href='../' class='homeBtn'>Back to Reading List</a>\
   </div>"
