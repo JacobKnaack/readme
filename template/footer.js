d626feffed33a68422a0b2e82495f504e5f1a89a
Jacob Knaack <jacobknck@gmail.com>
2018-02-20 16:52:24 -0800

    fixed footer url issue, and have continue anchor working

--- template/footer.js
+++ template/footer.js
@@ -1,6 +1,8 @@
-const url = window.location.url
+let idQuery = window.location.href.slice(-1);
+let nextArticle = parseInt(idQuery) + 1;
+let footerNext = window.location.href.slice(0, -1) + nextArticle;
 
 document.getElementById('footer').innerHTML = "\
   <a class='footerBtn back' href='../'><i class='fas fa-arrow-left'></i>reading list</a>\
-  <a class='footerBtn next' href=''>continue<i class='fas fa-arrow-right'></i></a>\
+  <a class='footerBtn next' href='"+ footerNext +"'>continue<i class='fas fa-arrow-right'></i></a>\
   "
