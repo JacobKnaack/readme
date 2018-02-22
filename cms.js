c065b0aecef839660e1b4fc715d8bdf9786eac8a
Jacob Knaack <jacobknck@gmail.com>
2018-02-21 17:02:10 -0800

    adding content to article 2

--- cms.js
+++ cms.js
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded', function() {
     if (document.getElementById("submissions")) {
       document.getElementById("submissions").innerHTML += "<li class='indexListItem'><p class='date'>"+articles[item].created_on+"</p><a id='article" + articles[item].id + "' class='listAnchor' href='"+url+"template/article.html?id=" + articles[item].id + "'><i class='fa fa-code indexListImg' class aria-hidden='true'></i>"+articles[item].name+"</a><p class='articleDescription'>"+articles[item].description+"</p></li>"
     }
-    if (url.includes('article') && parseInt(queryString) === articles[item].id) {
+    if (url.includes('article.html') && parseInt(queryString) === articles[item].id) {
       document.getElementById('articleTitle').innerHTML = articles[item].name;
       document.getElementById('createdOn').innerHTML = articles[item].created_on;
       document.getElementById('articleText').innerHTML = articles[item].html;
